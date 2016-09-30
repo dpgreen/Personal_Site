8b71133ce1c5c5287aaa99ef0a1dad67504e3257
Daniel Green <daniel_green@daniel-green.local>
2016-09-30 13:01:23 -0700

    Initial Push
    Initial Project Push

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+$action=$_REQUEST['action'];
+echo $action;
+$name=$_REQUEST['name'];
+$email=$_REQUEST['email'];
+$message=$_REQUEST['text'];
+
+if (($name=="")||($email=="")||($message==""))
+    {
+		echo "All fields are required, please fill <a href=\"\">the form</a> again.";
+	}
+else{		
+	    $from="From: $name<$email>\r\nReturn-path: $email";
+        $subject="Message sent using your contact form";
+		mail("youremail@yoursite.com", $subject, $message, $from);
+		echo "Email sent!";
+    }
+?>
