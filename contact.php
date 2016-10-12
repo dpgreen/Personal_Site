3332a0032436b485c25ddb8ed5c68b3b9f47e79a
dpgreen <daniel_green@daniel-green.local>
2016-10-11 21:25:02 -0700

    test
    test

--- contact.php
+++ contact.php
@@ -5,7 +5,7 @@ echo $action;
 $name=$_REQUEST['name'];
 $email=$_REQUEST['email'];
 $message=$_REQUEST['text'];
-echo "Test";
+echo $REQUEST;
 
 if (($name=="")||($email=="")||($message==""))
     {
