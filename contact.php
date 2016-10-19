f45fa0c0d013a788af48bf3f6ff4ca71007ec38b
dpgreen <daniel_green@daniel-green.local>
2016-10-19 10:04:30 -0700

    test
    test

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@ define('SENDER', 'dpgreen89@gmail.com');
 define('RECIPIENT', 'dpgreen89@gmail.com');
 define('USERNAME','AKIAIO33MXS74DQCTI7Q');
 define('PASSWORD','ArFM6IB+owV4Fh9evsGaODVaZZEOHcOeaD9p9Ze+dhK+');
-define('HOST', 'email-smtp.us-west-2.amazonaws.com');
+define('HOST', 'ssl://email-smtp.us-west-2.amazonaws.com');
 define('PORT', '587');  	
 define('SUBJECT','Incoming Inquiry from BreakingIntoProduct.com!');
 define('BODY','test send');
@@ -33,30 +33,33 @@ $message = "test";
 
 
 $result = $mail->send(RECIPIENT, $headers, BODY);
+echo("Email sent!"."\n");
+
+
 //$name = $_REQUEST['name'];
 //$email = $_REQUEST['email'];
 //$message = $_REQUEST['message'];
 
-if (($name=="")||($email=="")||($message==""))
-    {
-		echo "All fields are required, please fill <a href=\"\">the form</a> again.";
-	}
-else{		
+//if (($name=="")||($email=="")||($message==""))
+//    {
+//		echo "All fields are required, please fill <a href=\"\">the form</a> again.";
+//	}
+//else{		
 	    
 
  
 		//$mail = Mail::factory('smtp', $smtpParams);
 		//$result = $mail->send(RECIPIENT, $headers, BODY);
 
-		if (PEAR::isError($result)) {
-		  echo("Email not sent. " .$result->getMessage() ."\n");
-		} else {
-		  echo("Email sent!"."\n");
-		}
+//		if (PEAR::isError($result)) {
+//		  echo("Email not sent. " .$result->getMessage() ."\n");
+//		} else {
+//		  echo("Email sent!"."\n");
+//		}
 
 	    //$from="From: $name<$email>\r\nReturn-path: $email";
         //$subject="Message sent using your contact form";
 		//mail("dpgreen89@gmail.com", $subject, $message, $from);
 		//echo "Email sent!";
-    }
+  //  }
 ?>
